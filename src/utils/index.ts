dcd5051cc3b249dc2af50e02801ea8abca15de7c
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-01 19:42:49 +0100

    added util functions

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import getImage from './getImage';
+import createImageUrl from './createImageUrl';
+import writeThumbNail from './writeThumbNail';
+
+const utils = {
+  getImage,
+  createImageUrl,
+  writeThumbNail,
+};
+export default utils;
