35406511083a0b80cdc0c13139230651025bd064
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-01 23:11:27 +0100

    removed console.log

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -1,7 +1,6 @@
 import { existsSync, readFileSync } from 'fs';
 
 const getImage = (imageUrl: string) => {
-  console.log('getting image');
   if (!existsSync(imageUrl)) return null;
 
   const image = readFileSync(imageUrl);
