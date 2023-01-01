b8f8794a6f8f15826a378ad73858eb7cf35eab74
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-01 23:10:47 +0100

    refactored utility functions

--- src/routes/image-processing/routes.image-processing.ts
+++ src/routes/image-processing/routes.image-processing.ts
@@ -1,9 +1,41 @@
 import { Response, Router, Request } from 'express';
+import sharp from 'sharp';
+
+import utils from '../../utils';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('hello world from ' + `${req.query.j}`);
+router.get('/', async (req: Request, res: Response) => {
+  const { filename, height, width, type } = req.query;
+  const imageUrl = utils.createImageUrl(`${filename}.${type}`);
+  const image = utils.getImage(imageUrl);
+
+  if (!image) {
+    return res.end('file does not exist');
+  }
+
+  const newFilename = `${filename}_${height}X${width}.${type}`;
+  const thumbnailUrl = utils.createImageUrl(`/thumbnail/${newFilename}`);
+
+  // check if thumbnail exist
+  const thumbnail = utils.getImage(thumbnailUrl);
+
+  if (!thumbnail) {
+    // resize the image
+    const w = Number(width);
+    const h = Number(height);
+
+    const data = await sharp(imageUrl)
+      .resize(w, h)
+      .jpeg({ mozjpeg: true })
+      .toBuffer();
+
+    // then write the new thumbnail
+    utils.writeThumbNail(thumbnailUrl, data);
+    res.sendFile(thumbnailUrl);
+  } else {
+    res.sendFile(thumbnailUrl);
+  }
 });
 
 export default router;
---
+++ src/utils/createImageUrl.ts
@@ -0,0 +1,7 @@
+import { join, normalize, resolve } from 'path';
+
+const createImageUrl = (name: string): string => {
+  return normalize(join(resolve(__dirname, '../public'), name));
+};
+
+export default createImageUrl;
