4fdee4ef9829761de374ecfa3436aaa7857166e9
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-02 23:17:01 +0100

    expect case for type

--- src/tests/utils/imageProcessorSpec.ts
+++ src/tests/utils/imageProcessorSpec.ts
@@ -2,14 +2,16 @@ import sizeOf from 'buffer-image-size';
 import utils from '../../utils';
 
 describe('Process image for given sizes and type', () => {
-  it('should process given image and return it buffer with exact dimensions', async () => {
+  it('should process given image and return it buffer with exact type and dimensions', async () => {
     const imageUrl = utils.createImageUrl(`mouse.jpg`);
-    const w = 400;
-    const h = 400;
-    const image = await utils.imageProcesor('jpeg', imageUrl, w, h);
+    const w = 402;
+    const h = 403;
+    const type = 'png' as unknown;
+    const image = await utils.imageProcesor(type, imageUrl, w, h);
     const dimensions = sizeOf(image);
     expect(typeof image).toEqual(typeof Buffer.prototype);
     expect(dimensions.height).toBe(h);
     expect(dimensions.width).toBe(w);
+    expect(dimensions.type).toBe(type as string);
   });
 });
