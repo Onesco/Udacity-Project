1131e198575374dd78c81edda488f7e196dd514a
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-02 08:24:28 +0100

    added return type

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -1,6 +1,6 @@
 import { existsSync, readFileSync } from 'fs';
 
-const getImage = (imageUrl: string) => {
+const getImage = (imageUrl: string): Buffer | null => {
   if (!existsSync(imageUrl)) return null;
 
   const image = readFileSync(imageUrl);
